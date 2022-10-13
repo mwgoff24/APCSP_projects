be19b7c317ab890f51bea57c780c6bc07d43528a
Martin Goff <mwgoff24@students.bmchs.org>
2022-10-13 08:39:33 -0500

    worked more

--- unit1_num_conversion.py
+++ unit1_num_conversion.py
@@ -2,18 +2,31 @@
 # Martin Goff
 
 # functions to define
+from curses.ascii import isdigit
 
+
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def to_base_10(num, start_base):
     num_list = [int(d) for d in str(num)]
     new_num_list = []
     power = len(num_list)-1
+    letter_number = 10
     for item in num_list:
-        new_item = item * (start_base ** power)
-        print(f"{item}*{start_base}^{power}")
-        print(num_list)
-        new_num_list.append(new_item)
-        power -=1
+        if item.isdigit():
+            new_item = item * (start_base ** power)
+            print(f"{item}*{start_base}^{power}")
+            print(num_list)
+            new_num_list.append(new_item)
+            power -=1
+        else:
+            for character in letters:
+                if item == character:
+                    new_item = letter_number * (start_base ** power)
+                    new_num_list.append(new_item)
+                else:
+                    letter_number += 1
+
     print(new_num_list)
     print(sum(new_num_list))
 
@@ -22,13 +35,15 @@ def from_base_10(num, end_base):
     new_num = []
     while num // end_base != 0:
         remainder = num % end_base
-        print(remainder)
         new_num.append(str(remainder))
         num //= end_base
-    print(new_num)
+        if num // end_base == 0:
+            remainder = num % end_base
+            new_num.append(str(remainder))
+    new_num.reverse()
+    print(''.join(new_num))
     
 
-print(f"{15//16} R {15%16}")
 
 # program loop
 name = input("What is your name? ")
