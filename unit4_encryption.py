01dd24fe331a39d45af038df869dc988a2a200a1
Martin Goff <mwgoff24@students.bmchs.org>
2023-02-08 09:17:17 -0600

    can only shift one letter and append it to list infinitely

--- unit4_encryption.py
+++ unit4_encryption.py
@@ -3,30 +3,34 @@
 
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-shift = input("How much do you want to shift by? ")
+# shift = input("How much do you want to shift by? ")
 
-for item in letters:
-    new_index = letters.index(item)+int(shift)
-    if new_index > 25:
-        new_index -= 26
-    print(letters[new_index])
+# for item in letters:
+#     new_index = letters.index(item)+int(shift)
+#     if new_index > 25:
+#         new_index -= 26
+#     print(letters[new_index])
 
 unusable_chars = "/.,?';:!)("
 unusable_list = list(unusable_chars)
 
 def encrypt():
-    index = int(input("What do you want your index to be? Type in a number. "))
+    global letters
+    shift = int(input("How much do you want your shift to be? Type in a number. "))
     message = input("What is the message you want to use? ")
     message_list = list(message.lower())
     for character in message_list:
         if character in unusable_chars:
             message_list.remove(character)
-    new_letters = letters
-    for character in message_list:
         if character in letters:
-            letters.index(character)+index
-    print(message_list)
+            placement = letters.index(character)
+            placement += shift
+            message_list.insert(message_list.index(character), letters[placement])
+        print(message_list)
 
+
+def decrypt():
+    pass
 
 
 while True:
@@ -34,7 +38,6 @@ while True:
     if encrypt_or_decrypt == 'e':
         encrypt()
     elif encrypt_or_decrypt == 'd':
-        index = input("What do you want your index to be? Type in a number. ")
-        message = input("What is the message you want to use? ")
+        decrypt()
     else:
         print("I believe that is not an option.")
