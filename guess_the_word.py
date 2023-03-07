a7d66a44ac125b45beb2b4549b7469f46c36fac9
Martin Goff <mwgoff24@students.bmchs.org>
2023-03-07 08:40:58 -0600

    created word list, can choose random word from list

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,4 +1,13 @@
 import csv
+import random
 
 with open('word_list.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
+
+    word_list = []
+    for row in reader:
+        word_list.append(row['WORD'])
+
+    word = random.choice(word_list)
+    word = list(word)
+    print(word)
