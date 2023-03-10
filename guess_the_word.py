0681c67f81fac895da04bacd3c039c713d4f6dd2
Martin Goff <mwgoff24@students.bmchs.org>
2023-03-10 08:45:13 -0600

    got game loop going

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,3 +1,4 @@
+# guess the word
 import csv
 import random
 
@@ -7,8 +8,10 @@ with open('word_list.csv', newline='') as csvfile:
     word_list = []
     for row in reader:
         word_list.append(row['WORD'])
+    print(word_list)
 
-    word = random.choice(word_list)
+    word = word_list[0]
+    print(f"Your word is: {word}.")
     word = list(word)
     print(word)
 
@@ -16,3 +19,18 @@ with open('word_list.csv', newline='') as csvfile:
     for item in word:
         censored_word.append('*')
     print(censored_word)
+
+    lives = 5
+
+# game loop
+while True:
+    print(censored_word)
+    choice = input("Do you want to guess a letter or try to solve? ")
+    if choice == 'letter':
+        for item in word:
+            if item == choice:
+                censored_word[word.index(choice)] = choice
+                print("Well done! You guessed correctly!")
+            else:
+                print("Nope. You guessed wrong!")
+                lives -= 1
